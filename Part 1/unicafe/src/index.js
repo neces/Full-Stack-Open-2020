fb6e3ff8dc234944e0c1367e45cbaf0e4f9030a1
Neja's Mac <neja.cesnik@hotmail.com>
2020-08-30 14:06:19 +0200

    Solution 1.8

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Reviews = ({ text, number }) => <p>{text} {number}</p>
+
+const Statistics = ({ good, neutral, bad}) => {
+  let total = good + neutral + bad
+
+  return (
+  <>
+    <p>Total {total}</p>
+    <p>Average {(good - bad)/total}</p>
+    <p>Positive {((good/total)*100)} %</p>
+  </>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -10,46 +29,21 @@ const App = () => {
   const addGood = () => setGood(good + 1)
   const addNeutral = () => setNeutral(neutral + 1)
   const addBad = () => setBad(bad + 1)
-  
-  const Reviews = ({ text, number }) => <p>{text} {number}</p>
-
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-
-  const Total = ({ good, neutral, bad }) => <p>Total {good + neutral + bad}</p>
-
-  const Average = ({ good, neutral, bad }) => <p>Average {(good - bad)/(good + neutral + bad)}</p>
-
-  const Positive = ({ good, neutral, bad }) => <p>Positive {((good)/(good + neutral + bad)*100)} %</p>
 
   return (
     <div>
       <div>
         <h1>Give feedback</h1>
-        <Button
-          handleClick={addGood}
-          text='Good'
-        />
-        <Button
-          handleClick={addNeutral}
-          text='Neutral'
-        />
-        <Button
-          handleClick={addBad}
-          text='Bad'
-        />
+        <Button text='Good' handleClick={addGood} />
+        <Button text='Neutral' handleClick={addNeutral} />
+        <Button text='Bad' handleClick={addBad} />
 
         <h1>Statistics</h1>
         <Reviews text='Good' number={good} />
         <Reviews text='Neutral' number={neutral} />
         <Reviews text='Bad' number={bad} />
 
-        <Total good={good} neutral={neutral} bad={bad} />
-        <Average good={good} neutral={neutral} bad={bad} />
-        <Positive good={good} neutral={neutral} bad={bad} />
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   )
